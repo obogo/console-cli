4c0084e90f74e01e297f93e87904b0acee560561
wesjones <cybus10@gmail.com>
2017-03-07 16:14:34 -0700

    renamed to page-transition

--- templates/project/src/app/components/page/page.component.js
+++ templates/project/src/app/components/page/page.component.js
@@ -1,4 +1,4 @@
-module.directive('consolePage', function ($state) {
+module.directive('pageTransition', function ($state) {
     var prevState;
     var lastClass = '';
     var transition = '';
@@ -10,7 +10,7 @@ module.directive('consolePage', function ($state) {
             lastClass = transition;
             prevState = $state.current;
             // there is always two.
-            var all = document.querySelectorAll('ui-view console-page');
+            var all = document.querySelectorAll('ui-view page-transition');
             all[0].className = transition || 'anim-swap';
             all[1].className = transition || 'anim-swap';
         }
