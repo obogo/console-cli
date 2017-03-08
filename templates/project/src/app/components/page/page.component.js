c835a050af478013768de8d88edc4fab0983f17b
wesjones <cybus10@gmail.com>
2017-03-07 20:07:40 -0700

    renamed to page-transition

--- templates/project/src/app/components/page/page.component.js
+++ templates/project/src/app/components/page/page.component.js
@@ -11,8 +11,10 @@ module.directive('pageTransition', function ($state) {
             prevState = $state.current;
             // there is always two.
             var all = document.querySelectorAll('ui-view page-transition');
-            all[0].className = transition || 'anim-swap';
-            all[1].className = transition || 'anim-swap';
+            if (all[0] && all[1]) {
+                all[0].className = transition || 'anim-swap';
+                all[1].className = transition || 'anim-swap';
+            }
         }
     };
 });
