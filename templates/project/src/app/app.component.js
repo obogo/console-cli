513fdf996296da09024d020181d17432b6751d34
Robert Taylor <roboncode@gmail.com>
2017-03-08 06:44:13 -0700

    added menu backdrop

--- templates/project/src/app/app.component.js
+++ templates/project/src/app/app.component.js
@@ -1,9 +1,10 @@
 /* global angular, module */
-module.directive('app', function () {
+module.directive('app', function (Menu) {
     return {
         templateUrl: 'app',
         replace: true,
         link: function ($scope, $el, $attrs) {
+            $scope.menu = Menu;
         }
     };
 });
