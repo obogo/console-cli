1f46f7399494ae08ac1da00d5674eb9eb8570aa7
Robert Taylor <roboncode@gmail.com>
2017-03-10 08:04:58 -0700

    working on locale.service so we can bootstrap with it

--- templates/project/src/app/services/locale.service.js
+++ templates/project/src/app/services/locale.service.js
@@ -1,4 +1,4 @@
-/* global angular, obogo */
+/* global define, angular, obogo */
 module
     .value('locale', require('locale'))
     .value('localeConf', {
@@ -10,34 +10,55 @@ module
         supported: ['en-US', 'es-SP'],
         fallbacks: {'en':'en-US','sp':'es-SP'}
     });
-module.factory('LocaleService', function ($q, $http, $rootScope, locale, localeConf) {
-    var loaded;
-    function load(lang) {
-        var deferred = $q.defer();
-        if (lang && locale.$lang !== lang || !loaded) {
-            loaded = true;
-            locale.$config = localeConf;
-            locale.$lang = lang;
-            $http.get(locale.$url)
-                .then(function (response) {
-                    locale(response.data);
-                    deferred.resolve(locale);
-                }, function(e) {
-                    console.warn("Locales failed to load");
-                });
-        } else {
-            deferred.resolve(locale);
-        }
-        return deferred.promise;
+
+
+define('LocaleService', ["defer", 'http', 'extend'], function ($q, $http, $rootScope, locale, localeConf) {
+
+    function LocaleService() {
+
     }
 
-    var api = {
-        load: load // do it this way because it returns a promise for when it is loaded.
+    LocaleService.prototype.load = function(options) {
+        // {
+        //     localStorageKey: 'language',
+        //     basePath: 'languages',
+        //     defaultLocale: 'en-US',
+        //     fileExtension: '.lang.json',
+        //     persistLanguage: true,
+        //     supported: ['en-US', 'es-SP'],
+        //     fallbacks: {'en':'en-US','sp':'es-SP'}
+        // }
     };
-    Object.defineProperty(api, 'language', {
-        get: function() { return locale.$lang; },
-    });
-    load();
-    $rootScope.locale = locale;
-    return api;
+
+    return new LocaleService();
+
+    // var loaded;
+    // function load(lang) {
+    //     var deferred = $q.defer();
+    //     if (lang && locale.$lang !== lang || !loaded) {
+    //         loaded = true;
+    //         locale.$config = localeConf;
+    //         locale.$lang = lang;
+    //         $http.get(locale.$url)
+    //             .then(function (response) {
+    //                 locale(response.data);
+    //                 deferred.resolve(locale);
+    //             }, function(e) {
+    //                 console.warn("Locales failed to load");
+    //             });
+    //     } else {
+    //         deferred.resolve(locale);
+    //     }
+    //     return deferred.promise;
+    // }
+    //
+    // var api = {
+    //     load: load // do it this way because it returns a promise for when it is loaded.
+    // };
+    // Object.defineProperty(api, 'language', {
+    //     get: function() { return locale.$lang; },
+    // });
+    // load();
+    // $rootScope.locale = locale;
+    // return api;
 });
