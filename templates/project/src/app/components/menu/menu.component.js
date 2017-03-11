6bca5823b0afe77afac2f10b74787ca2ff5fc9fb
Robert Taylor <roboncode@gmail.com>
2017-03-11 07:29:32 -0700

    working on auth pages

--- templates/project/src/app/components/menu/menu.component.js
+++ templates/project/src/app/components/menu/menu.component.js
@@ -1,11 +1,16 @@
 module.component('consoleMenu', {
     templateUrl: 'menu',
-    controller: function (Menu, $transitions) {
+    controller: function (Menu, $transitions, $attrs) {
         var ctrl = this;
         ctrl.menu = Menu;
 
         $transitions.onCreate({ }, function(trans) {
             ctrl.state = trans.targetState().state().data;
+            if(ctrl.state.hideMenu) {
+                $attrs.$addClass('menu-hide');
+            } else {
+                $attrs.$removeClass('menu-hide');
+            }
             Menu.close();
         });
     }
